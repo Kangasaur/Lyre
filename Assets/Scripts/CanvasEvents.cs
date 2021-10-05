102d546136a97b4f44a9b7d8a8acc31fe7cf1b15
Kangasaur <itsakangasaur@gmail.com>
2021-10-04 23:39:38 -0400

    Added triggers and dialogue

---
+++ Assets/Scripts/CanvasEvents.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CanvasEvents : MonoBehaviour
+{
+    public GameObject letter, dialogueText, lyre, lyreSmall, continueText, oldMan, player;
+    string[] dialogue = new string[]
+    {
+        "You finally made it here.",
+        "You wanted to play the lyre, yes?",
+        "It is a good desire, and you are finally ready.",
+        "You have built up your body to be worthy of the responsibility of music.",
+        "Meanwhile, I am old, and this lyre has become mere company to me.",
+        "I will give it to you. I expect you will pick up its magic soon enough.",
+        "You will now forge your own path. Or rather...",
+        "You will take the path that the lyre shows you.",
+        "That is all. Take this, and go forth."
+    };
+    int diaIndex = 0;
+
+    private void Start()
+    {
+        dialogueText.GetComponent<TextMeshProUGUI>().text = dialogue[0];
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (letter.activeSelf)
+            {
+                letter.SetActive(false);
+                continueText.SetActive(false);
+                player.SendMessage("AllowMove");
+            }
+            else if (lyre.activeSelf)
+            {
+                lyre.SetActive(false);
+                continueText.SetActive(false);
+                player.SendMessage("AllowMove");
+            }
+            else if (dialogueText.activeSelf)
+            {
+                diaIndex++;
+                if (diaIndex == dialogue.Length)
+                {
+                    dialogueText.SetActive(false);
+                    continueText.SetActive(false);
+                    Destroy(oldMan);
+                    lyreSmall.SetActive(true);
+                    player.SendMessage("AllowMove");
+                }
+                else dialogueText.GetComponent<TextMeshProUGUI>().text = dialogue[diaIndex];
+            }
+        }
+    }
+
+    void ActivateLetter()
+    {
+        letter.SetActive(true);
+        continueText.SetActive(true);
+    }
+
+    void ActivateLyre()
+    {
+        lyre.SetActive(true);
+        continueText.SetActive(true);
+    }
+
+    void StartDialogue()
+    {
+        dialogueText.SetActive(true);
+        continueText.SetActive(true);
+    }
+}
--- Assets/Scripts/PlayInstrument.cs
+++ Assets/Scripts/PlayInstrument.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayInstrument : MonoBehaviour
 {
     public float speed = 3;
+    public GameObject canvas;
     public AudioSource voice1, voice2, voice3, voice4;
     public AudioClip g2, a2, b2, c3, d3, e3, f3, g3;
     int voice = 0;
-    bool hasLyre = true;
+    bool hasLyre = false;
+    bool canMove = false;
+    bool spellTrigger = false;
 
     Rigidbody2D myBody;
     float hmove, vmove;
@@ -16,11 +20,18 @@ public class PlayInstrument : MonoBehaviour
     private void Start()
     {
         myBody = GetComponent<Rigidbody2D>();
+        Scene scene = SceneManager.GetActiveScene();
+        if (scene.name == "Room 2")
+        {
+            hasLyre = true;
+            canMove = true;
+            spellTrigger = true;
+        }
     }
 
     void Update()
     {
-        DoMovement();
+        if (canMove) DoMovement();
 
         if (hasLyre) CheckInstrumentInput();
     }
@@ -68,5 +79,39 @@ public class PlayInstrument : MonoBehaviour
         }
         if (voice < 3) voice++;
         else voice = 0;
+    }
+
+    void AllowMove()
+    {
+        canMove = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch(collision.gameObject.name)
+        {
+            case "old man":
+                canMove = false;
+                myBody.velocity = Vector2.zero;
+                collision.gameObject.GetComponent<AudioSource>().Stop();
+                canvas.SendMessage("StartDialogue");
+                break;
+            case "Lyre Item":
+                canvas.SendMessage("ActivateLyre");
+                hasLyre = true;
+                canMove = false;
+                myBody.velocity = Vector2.zero;
+                Destroy(collision.gameObject);
+                break;
+            case "Room Portal":
+                SceneManager.LoadScene(1);
+                break;
+            case "Sun Song":
+                canvas.SendMessage("ActivateLetter");
+                Destroy(collision.gameObject);
+                canMove = false;
+                myBody.velocity = Vector2.zero;
+                break;
+        }
     }
 }
